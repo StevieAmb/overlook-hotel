198e8ed16d072edff0e8c8629cf8480e7dc5e188
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 10:19:05 -0700

    Commence user testing by using sample data

---
+++ test/User-test.js
@@ -0,0 +1,16 @@
+import chai from 'chai';
+const expect = chai.expect;
+
+describe('User', () => {
+  let user;
+  
+  beforeEach(() => {
+   newUser = new User()
+  });
+
+    it('Should be a function', () => {
+      expect(Recipe).to.be.a('function');
+    });
+
+  }
+    
