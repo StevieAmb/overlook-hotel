39fea26812942dd824f172c8bdca3e75e697bca2
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 11:41:31 -0700

    Create sample booking data for testing

---
+++ data/sample-booking-data.js
@@ -0,0 +1,39 @@
+const userBookingDdata = [
+  {
+  "id": "5fwrgu4i7k55hl6sz",
+  "userID": 9,
+  "date": "2022/04/22",
+  "roomNumber": 15,
+  "roomServiceCharges": []
+  },
+  {
+  "id": "5fwrgu4i7k55hl6t5",
+  "userID": 43,
+  "date": "2022/01/24",
+  "roomNumber": 24,
+  "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6t8",
+    "userID": 1,
+    "date": "2022/02/05",
+    "roomNumber": 12,
+    "roomServiceCharges": []
+  },
+  {
+  "id": "5fwrgu4i7k55hl6t7",
+  "userID": 20,
+  "date": "2022/02/16",
+  "roomNumber": 7,
+  "roomServiceCharges": []
+  },
+  {
+    "id": "5fwrgu4i7k55hl6tl",
+    "userID": 3,
+    "date": "2022/01/10",
+    "roomNumber": 8,
+    "roomServiceCharges": []
+    },
+]
+
+export default userBookingDdata;
