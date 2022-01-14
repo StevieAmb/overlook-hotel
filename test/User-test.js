0bfb9537a5b31f830f7d9c9aaa9c115e5376df9b
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 10:56:18 -0700

    Test for necessary properties of user class

--- test/User-test.js
+++ test/User-test.js
@@ -1,16 +1,31 @@
+import User from '../src/User';
+import userData from '../data/sample-user-data';
 import chai from 'chai';
 const expect = chai.expect;
 
 describe('User', () => {
   let user;
-  
   beforeEach(() => {
-   newUser = new User()
+    user = new User(userData[0])
   });
 
     it('Should be a function', () => {
-      expect(Recipe).to.be.a('function');
+      expect(User).to.be.a('function');
     });
 
-  }
+    it('Should be an instance of User', () => {
+      expect(user).to.be.an.instanceOf(User);
+    });
+
+    it('User should have an id', () => {
+      expect(user.id).to.equal(1);
+    });
+
+    it('User should have a name', () => {
+      expect(user.name).to.equal("Leatha Ullrich")
+    });
+
+
+
+  })
     
