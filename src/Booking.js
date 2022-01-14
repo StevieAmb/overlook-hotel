16297e4eae490fdb958c1bf77fc006ede434c7c5
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 11:46:59 -0700

    Complete properties for the booking class

--- src/Booking.js
+++ src/Booking.js
@@ -4,8 +4,10 @@ class Booking {
     this.userID = booking.userID;
     this.date = booking.date;
     this.roomNumber = booking.roomNumber;
-    this.roomServiceCharges = [];
+    this.roomServiceCharges = []; //this needs a method
   }
 }
+
+//method to get room service charges.
 
 export default Booking;
