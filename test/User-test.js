ffcc3120513af3f317852259e93f3983ef7d21f9
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 16:25:23 -0700

    Add test for methods in user class

--- test/User-test.js
+++ test/User-test.js
@@ -1,12 +1,25 @@
-import User from '../src/User';
 import userData from '../data/sample-user-data';
+import userRoomData from '../data/sample-room-data';
+import userBookingData from '../data/sample-booking-data';
+import Booking from '../src/Booking';
+import User from '../src/User';
 import chai from 'chai';
 const expect = chai.expect;
 
 describe('User', () => {
-  let user;
+  let user1;
+  let user2;
+  let booking1;
+  let booking2;
+  let booking3;
+  
   beforeEach(() => {
-    user = new User(userData[0])
+    user1 = new User(userData[3])
+    user2 = new User(userData[0])
+    console.log(user1)
+    booking1 = new Booking(userBookingData[2])
+    booking2 = new Booking(userBookingData[5])
+    booking3 = new Booking(userBookingData[1])
   });
 
     it('Should be a function', () => {
@@ -14,18 +27,36 @@ describe('User', () => {
     });
 
     it('Should be an instance of User', () => {
-      expect(user).to.be.an.instanceOf(User);
+      expect(user1).to.be.an.instanceOf(User);
     });
 
     it('User should have an id', () => {
-      expect(user.id).to.equal(1);
+      expect(user1.id).to.equal(userData[3].id);
     });
 
     it('User should have a name', () => {
-      expect(user.name).to.equal("Leatha Ullrich")
+      expect(user1.name).to.equal(userData[3].name)
     });
 
+    it('Should have a list of all the bookings the user has made', () => {
+      user1.findBookedRooms(booking1);
+      user1.findBookedRooms(booking2);
+  
+      expect(user1.roomsAlreadyBooked.length).to.equal(2)
+    });
 
+    it('Should inspire the user to book a room if they have not booked any', () => {
+      let message = user2.findBookedRooms(booking3)
+      expect(message).to.equal(`You haven't booked any rooms, yet! Get your vacation started with Overlook!`)
+    });
+
+    it('Should tell you how much a user has spent on all their bookings so far', () => {
+      user1.findBookedRooms(booking1);
+      user1.findBookedRooms(booking2);
+
+      let total = user1.getTotalSpentOnRooms(userRoomData);
+      expect(total).to.equal(836.58)
+    });
 
   })
     
