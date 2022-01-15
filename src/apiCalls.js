17eef077f91063579d4e7cc28ecdb049d326d139
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-14 17:22:35 -0700

    Create apiCall file

---
+++ src/apiCalls.js
@@ -0,0 +1,7 @@
+export const fetchApiData = (type) => {
+  return fetch(`http://localhost:3001/api/v1/${type}`)
+    .then((response) => 
+    response.json())
+    .catch(error => console.log('fetch error', error))
+}
+
