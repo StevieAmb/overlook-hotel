0604aeb53681e48868552531f29e85e80f7726eb
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-15 13:00:32 -0700

    Add methods to user class to check available rooms

--- src/User.js
+++ src/User.js
@@ -6,6 +6,7 @@ class User {
     this.name = user.name;
     this.roomsAlreadyBooked = []; //need a method for this
     this.totalSpent = 0;
+    this.availableBookings = [];
   }
 
   findBookedRooms = (bookings) => {
@@ -31,7 +32,33 @@ class User {
     }, 0)
     this.totalSpent = totalUserSpent;
   }
+
+  findBookingsByDate = (date, bookings) => {
+  bookings.forEach(booking => {
+    if(booking.date !== date) {
+      this.availableBookings.push(booking);
+    }
+  })
+  }
 }
+
+
+
+//PSEUDOCODE
+//Method One ()
+// the user is going to put in a date (year - month - day)
+// based on the date that the user puts in
+// it has to check against all of the bookings - Filter 
+// and then pull out the bookings that are available for that date
+
+// Method Two ()
+// From the list of bookings that are availble for that day (an array),
+// We are going to take in the roomsData as a parameter, 
+// and then we are going to have the other available booking
+// array in the method , and then if the room number of the booking 
+// matches the room numbers of the room Data being typed in - filter ,
+// then push out the available rooms
+
 
 
 
