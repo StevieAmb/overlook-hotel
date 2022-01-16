f191acb0122038c726cad8360a2152db9fe35f76
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-16 15:51:33 -0700

    Commence post function for posting bookings

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,7 +1,20 @@
-export const fetchApiData = (type) => {
+const fetchApiData = (type) => {
   return fetch(`http://localhost:3001/api/v1/${type}`)
     .then((response) => 
     response.json())
     .catch(error => console.log('fetch error', error))
 }
 
+const postBooking = () => {
+  return fetch('http://localhost:3001/api/v1/bookings', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      
+    })
+
+  })
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(err => console.log(err))
+}
