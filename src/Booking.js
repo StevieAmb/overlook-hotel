ed63eb0271df09cce73843acdc3f1a600fc242c2
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-16 01:01:13 -0700

    Refactor booking class

--- src/Booking.js
+++ src/Booking.js
@@ -1,13 +1,8 @@
 class Booking {
-  constructor(booking) {
-    this.id = booking.id;
-    this.userID = booking.userID;
-    this.date = booking.date;
-    this.roomNumber = booking.roomNumber;
-    this.roomServiceCharges = []; //this needs a method
+  constructor(bookings) {
+   this.bookings = bookings;
   }
 }
-
 
 
 //method to get room service charges.
