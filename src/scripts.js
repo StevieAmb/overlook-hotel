356423c1cef97f72ecdd1bd07b2c81a0a45a7172
Stevie Ambroise <s.ambroise@protonmail.com>
2022-01-17 17:18:29 -0700

    Update fetch call for single user and logging in

--- src/scripts.js
+++ src/scripts.js
@@ -2,7 +2,7 @@
 // Do not delete or rename this file ********
 
 // An example of how you tell webpack to use a CSS (SCSS) file
-import { fetchApiData, postBooking } from './apiCalls';
+import { fetchApiData, fetchSingleUser, postBooking } from './apiCalls';
 
 import './css/base.scss';
 
@@ -22,8 +22,8 @@ const filterRoomsButton = document.getElementById('filterByRoomType');
 let domButtons;
 
 //USER INPUTS
-const usernameInput = document.getElementById('username');
-const passwordInput = document.getElementById('loginButton');
+const usernameInput = document.getElementById('userName');
+const passwordInput = document.getElementById('password');
 const dateInput = document.getElementById('checkInCalendar')
 const selectedtRoomTypeDropdown = document.getElementById('roomTypeSelection')
 
@@ -51,23 +51,25 @@ let singleUser;
 
 function getData(userID) {
   return Promise.all([fetchSingleUser(userID), fetchApiData('rooms'), fetchApiData('bookings')])
-  .then(data => { 
-    [singleUser, roomsData, bookingsData] = [data[0], data[0].rooms, data[0].bookings]
-  });
 }
 
 //on click
-const loginUser = () => {
-  let userID = usernameInput.value.subStr(8);
+const loginUser = (event) => {
+  event.preventDefault();
+  let username = usernameInput.value
+  let userID = username.substring(8)
   if(userID > 0 && userID < 51 && passwordInput.value === 'overlook2021') {
-    getData(userID);
+  getData(userID)
+  .then(data => { 
+    [singleUser, roomsData, bookingsData] = [data[0], data[1].rooms, data[2].bookings]
     user = new User(singleUser)
+    console.log(user);
+      showUserDashboard();
+      console.log('rooms', roomsData)
+      displayUserDashBoard(user, roomsData, bookingsData)
+    });
   }
-  displayUserDashBoard(user, roomsData, bookingsData)
 }
-
-
-
 
 
 const getRandomIndex = (array) => {
@@ -115,18 +117,24 @@ const displayAvailableRooms = (event) => {
   user.findBookingsByDate(betterDate, bookingsData)
   user.findAvailableRooms(roomsData)
   availableRoomsView.innerHTML = ``;
-  user.availableRooms.forEach(availableRoom => {
-    availableRoomsView.innerHTML +=
-    `<section class="card-border" id="${availableRoom.number}">
-    <p>Room Number: ${availableRoom.number}</p>
-    <p>Room Type: ${availableRoom.roomType}</p>
-    <p>Has a bidet: ${availableRoom.bidet}</p>
-    <p>Bed size: ${availableRoom.bedSize}</p>
-    <p>Number of beds: ${availableRoom.numBeds}</p>
-    <p>Cost per night: ${availableRoom.costPerNight}</p>
-    <button class="book-room">Book This Room</button>
-    </section>`
-  })
+  if(user.availableRooms.length > 0) {
+    user.availableRooms.forEach(availableRoom => {
+      availableRoomsView.innerHTML +=
+      `<section class="card-border" id="${availableRoom.number}">
+      <p>Room Number: ${availableRoom.number}</p>
+      <p>Room Type: ${availableRoom.roomType}</p>
+      <p>Has a bidet: ${availableRoom.bidet}</p>
+      <p>Bed size: ${availableRoom.bedSize}</p>
+      <p>Number of beds: ${availableRoom.numBeds}</p>
+      <p>Cost per night: ${availableRoom.costPerNight}</p>
+      <button class="book-room">Book This Room</button>
+      </section>`
+    })
+  } else {
+    availableRoomsView.innerHTML = `
+    <p>Sorry, so sorry for the inconvenience! I beg of you, forgive us and
+    try your search again!</p>`
+  }
   domButtons = document.querySelectorAll('.book-room')
   clickBookButton(domButtons);
 }
@@ -137,19 +145,25 @@ const displayFilteredRooms = (event) => {
   showBookingRoomView();
   let wantedRooms = user.filterAvailableRooms(selectedtRoomTypeDropdown.value)
   bookedRoomView.innerHTML = ``;
-  wantedRooms.forEach(wantedRoom => {
-    bookedRoomView.innerHTML += `
-    <section class="card-border" id="${wantedRoom.number}">
-      <p>Room Number: ${wantedRoom.number}</p>
-      <p>Room Type: ${wantedRoom.roomType}</p>
-      <p>Has a bidet: ${wantedRoom.bidet}</p>
-      <p>Bed size: ${wantedRoom.bedSize}</p>
-      <p>Number of beds: ${wantedRoom.numBeds}</p>
-      <p>Cost per night: ${wantedRoom.costPerNight}</p>
-      <button class="book-room">Book This Room</button>
-    </section>
-    `
-  })
+  if(wantedRooms.length > 0) {
+    wantedRooms.forEach(wantedRoom => {
+      bookedRoomView.innerHTML += `
+      <section class="card-border" id="${wantedRoom.number}">
+        <p>Room Number: ${wantedRoom.number}</p>
+        <p>Room Type: ${wantedRoom.roomType}</p>
+        <p>Has a bidet: ${wantedRoom.bidet}</p>
+        <p>Bed size: ${wantedRoom.bedSize}</p>
+        <p>Number of beds: ${wantedRoom.numBeds}</p>
+        <p>Cost per night: ${wantedRoom.costPerNight}</p>
+        <button class="book-room">Book This Room</button>
+      </section>`
+    })
+  } else {
+    let message = user.filterAvailableRooms(selectedtRoomTypeDropdown.value)
+    console.log(message)
+    bookedRoomView.innerHTML = `
+    <p>${message}</p>`
+  }
   domButtons = document.querySelectorAll('.book-room')
   clickBookButton(domButtons);
 }
@@ -195,6 +209,11 @@ const hide = (elements) => {
 
 
 //VIEWS 
+const showUserDashboard = () => {
+  show([userCheckInView, navBar, userDashboard])
+  hide([availableRoomsView, bookedRoomView, loginView])
+}
+
 const showLoginPageView = () => {
   show([loginView]);
   hide([userDashboard, navBar, availableRoomsView, userCheckInView, bookedRoomView]);
@@ -222,7 +241,9 @@ const showBookingRoomView = () => {
 checkInButton.addEventListener('click', (event) => {
   displayAvailableRooms(event)
 });
-loginButton.addEventListener('click', loginUser)
+loginButton.addEventListener('click', (e) => {
+  loginUser(e)
+})
 filterRoomsButton.addEventListener('click', displayFilteredRooms)
 // homeButton.addEventListener('click', )
 
